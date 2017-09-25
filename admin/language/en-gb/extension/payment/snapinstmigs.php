b220d406759c4109108aa1d4dfa6ed6e2b6e2179
Harry <harry.pujianto@veritrans.co.id>
2017-09-25 11:22:34 +0700

    Add migs installment
    for bca/bri/maybank

---
+++ admin/language/en-gb/extension/payment/snapinstmigs.php
@@ -0,0 +1,40 @@
+<?php
+// Heading
+$_['heading_title']      = 'Snap Installment MIGS';
+
+// Text
+$_['text_extension']	 = 'Extensions';
+$_['text_payment']       = 'Payment';
+$_['text_success']       = 'Success: You have modified Snap Instalment MIGS configuration!';
+$_['text_snapinstmigs']     = '<a href="https://midtrans.com" target="_blank"><img src="view/image/payment/midtrans.png" width="120px" alt="Midtrans" title="Snap" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_live']          = 'production';
+$_['text_sandbox']       = 'sandbox';
+$_['text_successful']    = 'Always Successful';
+$_['text_fail']          = 'Always Fail';
+$_['text_edit']          = 'Configure Midtrans Snap Installment MIGS';
+
+// Entry
+$_['entry_environment']  = 'Environment'; // v2 API only
+$_['entry_client_key']   = 'Client Key'; // v2 API only
+$_['entry_server_key']   = 'Server Key'; // v2 API only
+$_['entry_order_status'] = 'Order Status';
+$_['entry_oneclick']     = 'One click';
+$_['entry_geo_zone']     = 'Geo Zone';
+$_['entry_status']       = 'Status';
+$_['entry_sort_order']   = 'Sort Order:';
+$_['entry_expiry']   	 = 'Custom Expire';
+$_['entry_custom_field'] = 'Custom Field';
+$_['entry_currency_conversion'] = 'Currency conversion to IDR';
+$_['entry_snapinst_success_mapping'] = 'Map Payment Success Status to Order Status:';
+$_['entry_snapinst_challenge_mapping'] = 'Map Payment Challenge Status to Order Status:';
+$_['entry_snapinst_failure_mapping'] = 'Map Payment Failure Status to Order Status:';
+$_['entry_display_name'] = 'Display name:';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify Snap installment MIGS!';
+$_['error_client_key']   = 'Client Key is required!';
+$_['error_server_key']   = 'Server Key is required!';
+$_['error_currency_conversion'] = 'Currency conversion rate is required when IDR currency is not installed in the system!';
+$_['error_display_name'] = 'Please specify a name for this payment method!';
+$_['error_min_txn']      = 'Please specify a minimum amount for installment!';
+?>
